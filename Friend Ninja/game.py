82d9295e4bca3a6035540a02b4725edbacc54eaa
Ashley May <aenmay@unc.edu>
2025-04-05 18:58:13 -0400

    note

--- Friend Ninja/game.py
+++ Friend Ninja/game.py
@@ -61,4 +61,103 @@ def draw_text(display, text, size, x, y):
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x,y)
     gameDisplay.blit(text_surface, text_rect)
+
+def draw_lives(display, x, y, lives, image) :
+    for i in range(lives) :
+        img = pygame.image.load(image)
+        img_rect = img.get_rect()
+        img_rect.x = int(x + 35 * 1)
+        img_rect.y = y
+        display.blit(img, img_rect)
+
+def show_gameover_screen():
+    gameDisplay.blit(background, (0,0))
+    draw_text(gameDisplay, "FRIEND NINJA!!", 90, WIDTH / 2, HEIGHT / 4)
+    if not game_over :
+        draw_text(gameDisplay, "Score : " + str(score), 50, WIDTH / 2, HEIGHT / 2)
+    
+    draw_text(gameDisplay, "Press a key to begin!", 64, WIDTH / 2, HEIGHT * 3 / 4)
+    pygame.display.flip()
+    waiting = True
+    while waiting:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYUP:
+                waiting = False
+
+
+first_round = True
+game_over = True
+game_running = True
+
+while game_running : 
+    if game_over: 
+        if first_round:
+            show_gameover_screen()
+            first_round = False
+        game_over = False
+        player_lives = 3
+        draw_lives(gameDisplay, 690,5, player_lives, 'images/heart.jpg')
+        score = 0
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_running = False
+    
+    gameDisplay.blit(background, (0,0))
+    gameDisplay.blit(score_text, (0,0))
+    draw_lives(gameDisplay, 690, 5, player_lives, 'images/heart.jpg')
+
+
+    for key, value in data.items():
+        if value['throw']:
+            value['x'] += value['speed_x']
+            value['y'] += value['speed_y']
+            value['speed_y'] += (1 * value['t'])
+            value['t'] += 1
+
+        if value['y'] <= 800:
+            gameDisplay.blit(value['img'], (value['x'], value['y']))
+            
+        else:
+            generate_random_friends(key)
+
+        current_position = pygame.mouse.get_pos()
+
+        if not value['hit'] and current_position[0] > value['x'] and current_position[0] < value['x']+60 \
+                    and current_position[1] > value['y'] and current_position[1] < value['y']+60:
+                if key == 'bomb':
+                    player_lives -= 1
+                    if player_lives == 0:
+
+                        hide_cross_lives(690, 15)
+                    elif player_lives == 1 :
+                        hide_cross_lives(725, 15)
+                    elif player_lives == 2 :
+                        hide_cross_lives(760, 15)
+                    
+                    if player_lives < 0 :
+                        show_gameover_screen()
+                        game_over = True
+
+                    half_fruit_path = "images/explosion.png"
+                else:
+                    half_fruit_path = "images/" + "half_" + key + ".png"
+
+                value['img'] = pygame.image.load(half_fruit_path)
+                value['speed_x'] += 10
+                if key != 'bomb' :
+                    score += 1
+                score_text = font.render('Score : ' + str(score), True, (255, 255, 255))
+                value['hit'] = True
+        else:
+            generate_random_friends(key)
+
+    pygame.display.update()
+    clock.tick(FPS)     
+pygame.quit
+
+
     
