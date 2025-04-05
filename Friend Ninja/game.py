9c6a945d6144f71e03f1004634eb3a6afc5c9d71
dangcao <dangcao@unc.edu>
2025-04-05 17:49:59 -0400

    first part

--- Friend Ninja/game.py
+++ Friend Ninja/game.py
@@ -1,3 +1,17 @@
 import pygame, sys
 import os
 import random
+
+lives = 3
+score = 0
+friends = ['thomas', 'caroline', 'asia', 'dylan', 'josh', 'paris', 'bomb']
+
+screen_width = 800
+screen_height = 500
+FPS = 20
+
+pygame.init()
+pygame.display.set_caption('Friend Ninja')
+gameDisplay = pygame.display.set_mode((screen_width, screen_height))
+
+clock = pygame.time.Clock()
