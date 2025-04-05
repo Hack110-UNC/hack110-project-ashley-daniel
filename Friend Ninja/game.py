f13aa39f3d08a06bb58fa7b96d44e2ede633d772
dangcao <dangcao@unc.edu>
2025-04-05 18:14:41 -0400

    kjqnfkjn2efn2kefknfekj2f

--- Friend Ninja/game.py
+++ Friend Ninja/game.py
@@ -15,3 +15,16 @@ pygame.display.set_caption('Friend Ninja')
 gameDisplay = pygame.display.set_mode((screen_width, screen_height))
 
 clock = pygame.time.Clock()
+
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
+background = pygame.image.load('images/background.jpg')
+font = pygame.font.Font(None, 42)
+score_count = font.reader('Score: ' + str(score), True, (255, 255, 255))
+lives_icon = pygame.image.load('images/heart.jpg')
