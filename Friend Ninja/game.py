61438a944f47a13e446ac7b50978b31585d7147d
dangcao <dangcao@unc.edu>
2025-04-05 18:35:42 -0400

    yo

--- Friend Ninja/game.py
+++ Friend Ninja/game.py
@@ -28,3 +28,37 @@ background = pygame.image.load('images/background.jpg')
 font = pygame.font.Font(None, 42)
 score_count = font.reader('Score: ' + str(score), True, (255, 255, 255))
 lives_icon = pygame.image.load('images/heart.jpg')
+
+def random_friends(friend):
+    friend_path = "images/" + friend + ".png"
+    data[friend] = {
+        'img': pygame.image.load(friend_path),
+        'x': random.randint(100,500),
+        'y': 800,
+        'speed_x': random.randint(-10,10),
+        'speed_y': random.randint(-80,-60),
+        'throw': False,
+        't': 0,
+        'hit' : False,
+    }
+    if random.random() >= 0.75:
+        data[friend]['throw'] = True
+    else:
+        data[friend]['throw'] = False
+
+data = {}
+for friend in friends:
+    generate_random_friends(friend)
+
+def hide_cross_lives(x, y):
+    gameDisplay.blit(pygame.imagge.load("images/empty_heart.png"), (x,y))
+
+
+font_name = pygame.font.match_font(None)
+def draw_text(display, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x,y)
+    gameDisplay.blit(text_surface, text_rect)
+    
